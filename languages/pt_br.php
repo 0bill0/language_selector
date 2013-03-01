99b32708570f54dafbb4ead6f51578e7101226a0
0bill0 <lessaguigui@hotmail.com>
2013-03-01 10:06:29 -0300

    Create pt_br.php
    Plugin file translation to Portuguese.

---
+++ languages/pt_br.php
@@ -0,0 +1,16 @@
+<?php
+/* Autor: Guilherme Luiz Lessa
+    http://resa.iff.edu.br
+  	Date: February, 2013
+		*/
+	$portugues_brasileiro = array(
+
+		'language_selector:admin:settings:min_completeness' => 'Mínimo de tradução completa (Exemplo: 30%)',
+		'language_selector:admin:settings:show_in_header' => 'Exibir seleção de idioma no cabeçalho?',
+		'language_selector:admin:settings:autodetect' => 'Habilitar detecção automática de idioma (para usuários não logado)',
+		'language_selector:admin:settings:show_images' => 'Exibir imagem do idioma/país (Se estiver disponível)',
+	
+	);
+					
+	add_translation("pt_br",$portugues_brasileiro);
+?>
